0aec8838ce8a1360ed51bd3077f18eaf97b3c226
Stanislav <stazilastas@gmail.com>
2023-10-03 09:50:29 -0500

    ClickerGame 1.0.0

--- ClickerGameStoreStajila/ViewController.swift
+++ ClickerGameStoreStajila/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var pointsOutlet: UILabel!
+    var points = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickAction(_ sender: Any) {
+        points += 1;
+        pointsOutlet.text = "\(points)"
+    }
+    
+    
+    @IBAction func goToStoreAction(_ sender: Any) {
+        performSegue(withIdentifier: "toStore", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var nvc = segue.destination as! ViewControllerStore
+        
+        nvc.storePoints = points
+    }
+    
 }
 
---
+++ ClickerGameStoreStajila/ViewControllerStore.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerStore.swift
+//  ClickerGameStoreStajila
+//
+//  Created by STANISLAV STAJILA on 10/3/23.
+//
+
+import UIKit
+
+class ViewControllerStore: UIViewController {
+
+    var storePoints: Int!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

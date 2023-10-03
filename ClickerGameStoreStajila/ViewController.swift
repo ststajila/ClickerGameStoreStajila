798cc037990ae64f8ebf91c3208c82c2816b0986
Stanislav <stazilastas@gmail.com>
2023-10-03 09:09:34 -0500

    Initial Commit

---
+++ ClickerGameStoreStajila/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClickerGameStoreStajila
+//
+//  Created by STANISLAV STAJILA on 10/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
